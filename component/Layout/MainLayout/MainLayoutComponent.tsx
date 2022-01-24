34c8dbae27aba54fd2e622bb577f95a8be661736
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-24 15:11:41 +0530

    Header Component added

--- component/Layout/MainLayout/MainLayoutComponent.tsx
+++ component/Layout/MainLayout/MainLayoutComponent.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { FooterComponent } from './Footers/FooterComponent';
-import { NavbarComponent } from './Navbars/NavbarComponent';
+import { FooterComponent } from './Footer/FooterComponent';
+import { HeaderComponent } from './Header/HeaderComponent';
+import { NavbarComponent } from './Navbar/NavbarComponent';
 
 export const MainLayoutComponent: React.FC<{}> = ({ children }) => {
   return (
     <>
       <NavbarComponent />
+      <HeaderComponent />
       {children}
       <FooterComponent />
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,33 +19,13 @@ const MyApp = ({ Component, pageProps }: AppProps | any) => {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
-        <title>Natours</title>
-
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <title>Natours | Exciting tours for adventurous people</title>
+        <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
       </Head>
-      <h1>Hello WORLD </h1>
       <LayoutContextProvider>
         <Layout>
           <Component {...pageProps} />
